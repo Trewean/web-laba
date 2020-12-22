8affd60d6068d174ca35c5154899615861cc5880
Trewean <71116135+Trewean@users.noreply.github.com>
2020-12-22 15:59:40 +0300

    Add files via upload

---
+++ block/navbar.php
@@ -0,0 +1,9 @@
+	<link rel="stylesheet" href="css/navbar.css">
+	<header>
+		<ul class ="menu-main">
+			<li><a href="site.php" title="Главная" >Главная</a></li>			
+			<li><a href="Trades.php" title="Номенклатура" >Товары на складе</a></li>
+			<li><a href="#" title="Ответы на вопросы" >FAQ</a></li>
+			<li><a href="Enter.php">Личный кабинет</a></li>
+		</ul>
+	</header>
