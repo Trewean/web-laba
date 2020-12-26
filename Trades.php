12d574f8cdd38ca1cb7389f29cabffddb1414909
Trewean <71116135+Trewean@users.noreply.github.com>
2020-12-26 17:33:02 +0300

    Add files via upload

---
+++ Trades.php
@@ -0,0 +1,39 @@
+<?php  session_start();?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="css/Product.css">
+    <title>Товары</title>
+  </head>
+  <body>
+    <?php require('block\navbar.php') ?>
+    <?php require('Script\array.php') ?>
+    <?php
+    $i = 0; 
+    $n = count($arr);
+    while($i < $n){ ?>
+      <div class="line">
+    <?php 
+    $k = 4;
+    if($n - $i < 4) $k = $n - $i;
+    for($j = 0; $j < $k; $j++){
+      $trade = $arr[$i];
+      ?>
+      <div class="card">
+        <img src="Trades/<?php echo htmlspecialchars($trade['image'])?>" alt="">
+        <h3><?php echo htmlspecialchars($trade['name']) ?></h3>
+        <p><?php echo htmlspecialchars($trade['price']) ?> р/штука</p>
+        <p><?php echo htmlspecialchars($trade['quantity']) ?> на складе</p>
+      </div>
+      <?php
+      $i++;
+      }  ?>
+    </div>
+    <?php } ?>
+    <?php if($_SESSION['login'] == "Admin"){ ?>
+    <button>
+      <a class="button" href="CRUD.php">Изменить товары</a>
+    </button>
+  <?php } ?>
+  </body>
+</html>
---
+++ private_cabinet.php
@@ -0,0 +1,61 @@
+ <?php 
+ session_start(); 
+ $user = $_SESSION['user'];
+ $connect = mysqli_connect('localhost', 'root', '', 'users');
+ $result1 = mysqli_query($connect, "SELECT * FROM `discont` WHERE `user` = '$user'");
+ $result2 = mysqli_query($connect, "SELECT * FROM `cargo` WHERE `user` = '$user'");
+ $result3 = mysqli_query($connect, "SELECT * FROM `cargo` WHERE `user` = '$user'");
+ ?>
+ <html lang="ru">
+  <head>
+    <link rel="stylesheet" href="css/cabinet.css">  
+    <title>Личный кабинет</title>
+  </head>
+  <body>
+  	<?php if($_SESSION['cabinet'] == 0){
+  		header('Location: ../site.php');
+  	} 
+  	else {
+      $row = mysqli_fetch_row($result3);
+      ?>
+    <?php require('block\navbar.php') ?>
+    <div class="inform">
+      <div class="name">
+        <h3>Здраствуйте</h3> <?php echo htmlspecialchars($_SESSION['user']); ?><br>
+        <h3>Логин: </h3> <?php echo htmlspecialchars($_SESSION['login']); ?>
+      </div>
+    </div>
+        <div class="inform">
+          <div class="cargo">
+            <h3>Ваши грузы:</h3><br>
+            <h4><?php  if($row == NULL){ echo "Тут пока ничего нет";?></h4>
+            <a href="Trades.php"><button type="button" class="btn btn-primary">Заказать</button></a>
+              <?php } else{?>
+                <?php  while($pow2 = mysqli_fetch_assoc($result2)){?>
+                  <p>
+                    <h4>Груз из: 
+                      <span class="in">
+                        <?php             
+                          echo htmlspecialchars($pow2['cargo']);
+                         ?></span>
+                    Дата заказа: <span class="in">
+                      <?php echo htmlspecialchars($pow2['time']); ?>
+                      </span></h4>  
+                  </p>
+              <?php }}} ?>
+          </div>
+          <div class="discont">
+            <h3>Ваша скидка равна :
+              <span class="in">
+                <?php 
+                  $pow1 = mysqli_fetch_assoc($result1);
+                  echo htmlspecialchars($pow1['discont']);
+                ?></span>
+            </h3>
+          </div>
+        </div>
+              <button >
+                  <a class="button" href="script/exit.php">Выход</a>
+              </button>  
+  </body>
+</html>
